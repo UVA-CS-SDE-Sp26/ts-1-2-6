5ffa0587e81d96794898c800f053c35f27f20a8b
Malisha Das <malishadas@Malishas-MacBook-Air.local>
2026-02-11 03:22:53 -0500

    program control

---
+++ src/main/java/ProgramControl.java
@@ -0,0 +1,48 @@
+import java.nio.file.Path;
+import java.util.List;
+
+public class ProgramControl {
+    private DefaultFileHandler fileHandler;
+
+    public ProgramControl(DefaultFileHandler fileHandler) {
+        this.fileHandler = fileHandler;
+    }
+    public String requestControl(String[] arguments){
+        List<String> files = fileHandler.listFiles();
+        if (files.isEmpty() || files == null){
+            return "No such file or directory";
+        }
+
+        if (arguments.length == 0) { //this is default, no argument, prints all the files in the directory
+            String result = "Files in directory: ";
+            int indexOfFiles = 1;
+            for (String file : files) {
+                result += indexOfFiles + ". " + file + "\n";
+                indexOfFiles++;
+            }
+            return result;
+        }
+
+        String requested = arguments[0];
+
+            try{ //this sees if the argument entered is a number, if so, it pulls content through the file number
+                int fileNumReq = Integer.parseInt(requested);
+                if (fileNumReq < 1 || fileNumReq > files.size()) {
+                    return ("Invalid argument There isn't a file with this number");
+                }
+                String fileSelected = files.get(fileNumReq - 1); //-1 for how things are indexed in java
+                String content = fileHandler.readFile(fileSelected);
+                return (content);
+            } catch(NumberFormatException e) {} //goes to file name version because it wasn't a number
+
+            String content = "";
+            if (files.contains(requested)) { //this is the file name version
+                content = fileHandler.readFile(requested);
+                return (content);
+            }
+            else {
+                return ("File does not exist. Please enter a valid file name.");
+            }
+    }
+
+}
